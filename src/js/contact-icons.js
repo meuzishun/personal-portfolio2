383dea58f4f20207ee4ba81c529f61ffd44d7236
Andrew Smith <meuzishun@gmail.com>
2022-12-16 14:16:19 -0500

    Add html, styles, and svg
    Copy and paste the html for now. Structure the styles as they were but use small js files to bring in the svgs.

---
+++ src/js/contact-icons.js
@@ -0,0 +1,15 @@
+import githubIconFile from '../assets/icons/contact/github-original.svg';
+import linkedinIconFile from '../assets/icons/contact/linkedin-original.svg';
+import twitterIconFile from '../assets/icons/contact/twitter-original.svg';
+
+const contactIcons = [githubIconFile, linkedinIconFile, twitterIconFile];
+
+const linksContainer = document.querySelector('.links');
+
+contactIcons.forEach((file) => {
+  const icon = document.createElement('object');
+  icon.classList.add('icon');
+  icon.type = 'image/svg+xml';
+  icon.data = file;
+  linksContainer.appendChild(icon);
+});
---
+++ src/js/tech-icons.js
@@ -0,0 +1,39 @@
+import htmlIconFile from '../assets/icons/tech/html5-original.svg';
+import cssIconFile from '../assets/icons/tech/css3-original.svg';
+import jsIconFile from '../assets/icons/tech/javascript-original.svg';
+import gitIconFile from '../assets/icons/tech/git-original.svg';
+import npmIconFile from '../assets/icons/tech/npm-original-wordmark.svg';
+import sassIconFile from '../assets/icons/tech/sass-original.svg';
+import reactIconFile from '../assets/icons/tech/react-original.svg';
+import nodejsIconFile from '../assets/icons/tech/nodejs-original.svg';
+import jestIconFile from '../assets/icons/tech/jest-plain.svg';
+
+const techIconsFile = [
+  htmlIconFile,
+  cssIconFile,
+  jsIconFile,
+  gitIconFile,
+  npmIconFile,
+  sassIconFile,
+  reactIconFile,
+  nodejsIconFile,
+  jestIconFile,
+];
+
+const iconsContainer = document.querySelector('.tech-icons');
+
+techIconsFile.forEach((file) => {
+  const icon = document.createElement('object');
+  icon.classList.add('icon');
+  icon.type = 'image/svg+xml';
+  icon.data = file;
+  iconsContainer.appendChild(icon);
+});
+
+techIconsFile.forEach((file) => {
+  const icon = document.createElement('object');
+  icon.classList.add('icon');
+  icon.type = 'image/svg+xml';
+  icon.data = file;
+  iconsContainer.appendChild(icon);
+});
