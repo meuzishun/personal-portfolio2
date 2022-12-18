0f5663fcd5f493ba6e15cd0c26ba6452f47aba85
Andrew Smith <meuzishun@gmail.com>
2022-12-18 12:00:47 -0500

    Improve layout for both header and footer

--- src/js/sections/footer.js
+++ src/js/sections/footer.js
@@ -4,15 +4,19 @@ import { linksContainer } from '../components/contact-icons.js';
 const mainFooter = document.createElement('footer');
 mainFooter.classList.add('main-footer');
 
+const callToAction = document.createElement('div');
+callToAction.classList.add('call-to-action');
+
 const heading2 = document.createElement('h2');
 heading2.textContent = 'Contact me';
+callToAction.appendChild(heading2);
 
-const callToAction = document.createElement('p');
-callToAction.classList.add('call-to-action');
-callToAction.textContent =
+const blurb = document.createElement('p');
+blurb.classList.add('call-to-action');
+blurb.textContent =
   'If you enjoy my work and would like to discuss potentially working together, please feel free to contact me.';
+callToAction.appendChild(blurb);
 
-mainFooter.appendChild(heading2);
 mainFooter.appendChild(callToAction);
 mainFooter.appendChild(emailContainer);
 mainFooter.appendChild(linksContainer);
