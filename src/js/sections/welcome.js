7f198bf92627bb9f25e11b4c4419aaad6335a040
Andrew Smith <meuzishun@gmail.com>
2022-12-25 09:42:57 -0500

    Wrap about in header for animation layout

--- src/js/sections/welcome.js
+++ src/js/sections/welcome.js
@@ -1,12 +1,12 @@
 // import { header } from '../components/header.js';
-import { about } from '../components/about.js';
+// import { about } from '../components/about.js';
 import { iconsContainer } from '../components/tech-icons.js';
 
 const welcomeSection = document.createElement('section');
 welcomeSection.classList.add('welcome-section');
 welcomeSection.id = 'top';
 // welcomeSection.appendChild(header);
-welcomeSection.appendChild(about);
+// welcomeSection.appendChild(about);
 welcomeSection.appendChild(iconsContainer);
 
 export { welcomeSection };
