c1e8271bf622ac8cc3128bc13ea67e775238f17f
Andrew Smith <meuzishun@gmail.com>
2022-12-30 13:23:08 -0500

    Move about to welcome

--- src/js/components/header.js
+++ src/js/components/header.js
@@ -1,10 +1,8 @@
 import { navbar } from './navbar.js';
-import { about } from './about.js';
 
 const header = document.createElement('header');
 header.classList.add('main-header');
 
 header.appendChild(navbar);
-header.appendChild(about);
 
 export { header };
--- src/js/sections/welcome.js
+++ src/js/sections/welcome.js
@@ -1,8 +1,10 @@
 import { iconsContainer } from '../components/tech-icons.js';
+import { about } from '../components/about.js';
 
 const welcomeSection = document.createElement('section');
 welcomeSection.classList.add('welcome-section');
 welcomeSection.id = 'top';
 welcomeSection.appendChild(iconsContainer);
+welcomeSection.appendChild(about);
 
 export { welcomeSection };
