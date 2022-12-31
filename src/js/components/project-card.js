58acb58379adef5b7bf8ca797dd8b42eb57fbd99
Andrew Smith <meuzishun@gmail.com>
2022-12-31 13:55:30 -0500

    not doing what I want, back to main

--- src/js/components/project-card.js
+++ src/js/components/project-card.js
@@ -6,16 +6,20 @@ projectTitle.classList.add('project-title');
 projectTitle.textContent = 'My Awesome Project';
 projectCard.appendChild(projectTitle);
 
+const projectLinks = document.createElement('div');
+projectLinks.classList.add('project-links');
+projectCard.appendChild(projectLinks);
+
 const projectLiveLink = document.createElement('a');
 projectLiveLink.classList.add('project-live-link');
 projectLiveLink.href = 'www.google.com';
 projectLiveLink.textContent = 'Live Site';
-projectCard.appendChild(projectLiveLink);
+projectLinks.appendChild(projectLiveLink);
 
 const projectRepoLink = document.createElement('a');
 projectRepoLink.classList.add('project-repo-link');
 projectRepoLink.href = 'www.github.com';
 projectRepoLink.textContent = 'Repo';
-projectCard.appendChild(projectRepoLink);
+projectLinks.appendChild(projectRepoLink);
 
 export { projectCard };
