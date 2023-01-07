752bd721101edd4bb233de57a9f96bee12722960
Andrew Smith <meuzishun@gmail.com>
2023-01-06 19:51:16 -0500

    Move tech icons to about component

--- src/js/components/about.js
+++ src/js/components/about.js
@@ -1,3 +1,5 @@
+import { iconsContainer } from './tech-icons.js';
+
 const about = document.createElement('div');
 about.classList.add('about');
 about.classList.add('hidden');
@@ -10,5 +12,6 @@ const blurb = document.createElement('p');
 blurb.textContent =
   'I am a well-dress man sitting at a desk. I am signaling with my eye contact and body language that I am engaged and listening closely to the person talking to me. Or maybe I am in deep thought about how to make the company more profitable. Either way, you can trust and rely on me. Plus my tie is a non-threatening cornflower-blue which conveys a relaxed and welcoming demeanor.';
 about.appendChild(blurb);
+about.appendChild(iconsContainer);
 
 export { about };
--- src/js/sections/welcome.js
+++ src/js/sections/welcome.js
@@ -1,10 +1,10 @@
-import { iconsContainer } from '../components/tech-icons.js';
+// import { iconsContainer } from '../components/tech-icons.js';
 import { about } from '../components/about.js';
 
 const welcome = document.createElement('section');
 welcome.classList.add('welcome');
 welcome.id = 'welcome';
-welcome.appendChild(iconsContainer);
+// welcome.appendChild(iconsContainer);
 welcome.appendChild(about);
 
 export { welcome };
